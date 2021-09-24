f86998d14793da06959ec09f14d843c430e95510
goyalpratik807 <66640975+goyalpratik807@users.noreply.github.com>
2021-09-24 16:33:09 +0530

    Update client.js

--- www/js/client.js
+++ www/js/client.js
@@ -1037,7 +1037,7 @@ function setupLocalMedia(callback, errorback) {
       // https://blog.addpipe.com/common-getusermedia-errors/
       console.error("Access denied for audio/video", err);
       playSound("error");
-      //window.location.href = `/permission?roomId=${roomId}&getUserMediaError=${err}`;
+      window.location.href = `/permission?roomId=${roomId}&getUserMediaError=${err}`;
       if (errorback) errorback();
     });
 } // end [setup_local_stream]
